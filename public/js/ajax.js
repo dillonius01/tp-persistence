f6808425a9ff6d0ba6be374e93ff2c3138a80d08
Brandon Culp <bculp513@gmail.com>
2016-10-06 12:14:30 -0400

    loading hotel data for day 1

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -6,6 +6,7 @@ $.get('/api/hotels')
 		$('#hotel-choices').append(hotelHtml);
 	})
 })
+.catch(console.error);
 
 //setup option tags for restaurants
 $.get('/api/restaurants')
@@ -15,6 +16,7 @@ $.get('/api/restaurants')
 		$('#restaurant-choices').append(restaurantHtml);
 	})
 })
+.catch(console.error);
 
 //setup option tags for activities
 $.get('/api/activities')
@@ -24,4 +26,51 @@ $.get('/api/activities')
 		$('#activity-choices').append(activityHtml);
 	})
 })
+.catch(console.error);
 
+//places buttons on page for each day in db
+$.get('/api/days')
+.then(function(days) {
+	//sort days by number so buttons show up correctly ordered
+	days.sort(function(day1, day2) {
+		return day1.number - day2.number;
+	});
+	days.forEach(function(day) {
+		var buttonHtml = $('<button></button').addClass('btn btn-circle day-btn')
+		.text(day.number)
+		buttonHtml.insertBefore($('#day-add'));
+	})
+	//highlights first day button as current day
+	var firstDay = $('.day-buttons button:first-child');
+	firstDay.addClass('current-day');
+
+	//pull up data for day 1 when page is initially loaded
+	var dayOneData = days[0];
+	renderAttractions(dayOneData)
+
+})
+.catch(console.error);
+
+//pulls up data for each day when that btn is clicked
+function renderAttractions(day) {
+	$.get('/api/hotels')
+	.then(function(hotels) {
+		var hotelName = hotels.find(function(hotel) {
+			return hotel.id === day.hotelId
+		}).name
+		//render html with hotel name
+		var hotelHtml = $('<div class="itinerary-item"> \
+			<span class="title">'+ hotelName +'</span>\
+            <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>')
+		$('ul[data-type=hotel]').append(hotelHtml);
+	})
+	.catch(console.error);
+
+
+}
+
+//add hotel to My Hotel & day table as hotelID
+
+//add restaurant to My restaurants & dayrestaurants
+
+//add activity to My activities & dayactivities
